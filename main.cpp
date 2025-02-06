4176e77cb28b13cfd1d8aaf625ecc2c99725f987
RAZAFITRIMO Tsifeherana Miranto <167677282+Tsifeherana@users.noreply.github.com>
2025-02-06 17:04:09 +0300

    Add files via upload
    example of application using all classes and objects

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "namespace.h"
+#include <iostream>
+using namespace isa;
+int main()
+{
+    Rationnel a;
+    Rationnel b;
+    a.setNumerateur(2);
+    a.setDenominateur(3);
+    b.setNumerateur(1);
+    b.setDenominateur(2);
+    Compra c;
+    c.setReel(a);
+    c.setIm(b);
+    Rationnel e;
+    Rationnel f;
+    e.setNumerateur(2);
+    e.setDenominateur(3);
+    f.setNumerateur(1);
+    f.setDenominateur(2);
+    Compra(d);
+    d.setReel(e);
+    d.setIm(f);
+    c.multiplicationRC(d).afficherRC();
+
+    //arithmetic;
+    float sum;
+    sum = Arithmetic::add(float(2.5),a);
+    std::cout << sum << std::endl;
+    
+    return 0;
+}
